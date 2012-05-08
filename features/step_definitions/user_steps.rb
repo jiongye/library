692bb80cc511abd3921c300bab6fe2ce721d4d5c
Jiongye Li <jiongye@gmail.com>
2012-05-07 22:00:34 -0400

    fixed cucumber test

--- features/step_definitions/user_steps.rb
+++ features/step_definitions/user_steps.rb
@@ -10,13 +10,6 @@ def find_user
   @user ||= User.first conditions: {:username => @visitor[:username]}
 end
 
-def create_unconfirmed_user
-  create_visitor
-  delete_user
-  sign_up
-  visit '/users/sign_out'
-end
-
 def create_user
   create_visitor
   delete_user
@@ -26,18 +19,6 @@ end
 def delete_user
   @user ||= User.first conditions: {:username => @visitor[:username]}
   @user.destroy unless @user.nil?
-end
-
-def sign_up
-  delete_user
-  visit '/users/sign_up'
-  fill_in "Name", :with => @visitor[:name]
-  fill_in "Username", :with => @visitor[:username]
-  fill_in "Email", :with => @visitor[:email]
-  fill_in "Password", :with => @visitor[:password]
-  fill_in "Password confirmation", :with => @visitor[:password_confirmation]
-  click_button "Sign up"
-  find_user
 end
 
 def sign_in
@@ -66,10 +47,6 @@ Given /^I do not exist as a user$/ do
   delete_user
 end
 
-Given /^I exist as an unconfirmed user$/ do
-  create_unconfirmed_user
-end
-
 ### WHEN ###
 When /^I sign in with valid credentials$/ do
   create_visitor
@@ -78,35 +55,6 @@ end
 
 When /^I sign out$/ do
   visit '/users/sign_out'
-end
-
-When /^I sign up with valid user data$/ do
-  create_visitor
-  sign_up
-end
-
-When /^I sign up with an invalid email$/ do
-  create_visitor
-  @visitor = @visitor.merge(:email => "notanemail")
-  sign_up
-end
-
-When /^I sign up without a password confirmation$/ do
-  create_visitor
-  @visitor = @visitor.merge(:password_confirmation => "")
-  sign_up
-end
-
-When /^I sign up without a password$/ do
-  create_visitor
-  @visitor = @visitor.merge(:password => "")
-  sign_up
-end
-
-When /^I sign up with a mismatched password confirmation$/ do
-  create_visitor
-  @visitor = @visitor.merge(:password_confirmation => "please123")
-  sign_up
 end
 
 When /^I return to the site$/ do
@@ -123,17 +71,6 @@ When /^I sign in with a wrong password$/ do
   sign_in
 end
 
-When /^I edit my account details$/ do
-  click_link "Edit account"
-  fill_in "Name", :with => "newname"
-  fill_in "Current password", :with => @visitor[:password]
-  click_button "Update"
-end
-
-When /^I look at the list of users$/ do
-  visit '/'
-end
-
 ### THEN ###
 Then /^I should be signed in$/ do
   page.should have_content "Logout"
@@ -142,21 +79,12 @@ Then /^I should be signed in$/ do
 end
 
 Then /^I should be signed out$/ do
-  page.should have_content "Sign up"
   page.should have_content "Login"
   page.should_not have_content "Logout"
 end
 
-Then /^I see an unconfirmed account message$/ do
-  page.should have_content "You have to confirm your account before continuing."
-end
-
 Then /^I see a successful sign in message$/ do
   page.should have_content "Signed in successfully."
-end
-
-Then /^I should see a successful sign up message$/ do
-  page.should have_content "Welcome! You have signed up successfully."
 end
 
 Then /^I should see an invalid email message$/ do
@@ -183,11 +111,4 @@ Then /^I see an invalid login message$/ do
   page.should have_content "Invalid username or password."
 end
 
-Then /^I should see an account edited message$/ do
-  page.should have_content "You updated your account successfully."
-end
 
-Then /^I should see my name$/ do
-  create_user
-  page.should have_content @user[:name]
-end
