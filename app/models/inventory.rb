9f50f2e1586067bddfbda9fd869d6c603bdaaa6d
Jiongye Li <jiongye@gmail.com>
2013-04-14 10:35:47 -0400

    fixed a bug

--- app/models/inventory.rb
+++ app/models/inventory.rb
@@ -2,7 +2,7 @@ class Inventory < ActiveRecord::Base
   belongs_to :book
   attr_accessible :borrow_out, :quantity, :book_id, :shelve_quantity
 
-  validates_presence_of :quantity, :book_id
+  validates_presence_of :quantity
 
   def in_stock
     quantity - borrow_out.to_i
